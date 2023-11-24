33d28463828d25fb1ab92af2120ffb1dc80dca1d
Vahe Zilabyan <zilabyan@synopsys.com>
2023-11-24 12:19:19 +0400

    changes

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -11,6 +11,7 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+    this->setWindowTitle("Duplicate Detector");
 }
 
 void MainWindow::set_text_in_textEdit(QString filename)
